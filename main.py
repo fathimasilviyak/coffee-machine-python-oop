3d1476526873da3fe2b3d1e2c1d9c16a660da8f0
fathimasilviyak <silviyakvm@gmail.com>
2023-03-22 13:02:16 +0530

    show the report if the user want it

--- main.py
+++ main.py
@@ -9,4 +9,12 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
 
+is_on = True
+while is_on:
+    user_input = input(f"What would you like? : {menu.get_items()}").lower()
+    if user_input == "report":
+        coffee_maker.report()
+        money_machine.report()
+ 
+
 
