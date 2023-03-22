17859a6bb971d2e4113af7d58793585c929e7762
fathimasilviyak <silviyakvm@gmail.com>
2023-03-22 12:09:24 +0530

    change made in main.py

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 # create object for each classes
-menu =  Menu()
-menu_item = MenuItem()
+menu = Menu()
+espresso = MenuItem("espresso", 50, 18, 1.5)
+latte = MenuItem("latte", 200, 150, 24, 2.5)
+cappuccino = MenuItem("cappuccino", 250, 100, 24, 3)
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
+user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+if user_input == "report":
+    report = CoffeeMaker()
+    print(report)
