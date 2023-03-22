4022699fbc7f6a072711ae856fa5d3ff660207f6
fathimasilviyak <silviyakvm@gmail.com>
2023-03-22 13:00:01 +0530

    created objects for the classes

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# create object for each classes
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+
